c5bc60e0b833a520f1c37352da3fe2c058f8b2e7
Ryan Tsihugwa <101879237+Tsihugwa@users.noreply.github.com>
2023-09-07 12:13:32 +0100

    Add files via upload

---
+++ Advanced C# OOP/CET212-UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CET212-UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CET212-UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5c448ddd-2f17-4b00-8274-29a6257a6d1e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Advanced C# OOP/CET212-UnitTest/UnitTest1.cs
@@ -0,0 +1,87 @@
+﻿using CET212_Assessment;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+[TestClass]
+public class PCQueueTests
+{
+    [TestMethod]
+    public void EnqueueItemAndDequeueItem_WithUnboundedCapacity_ShouldSucceed()
+    {
+        // Arrange
+        var queue = new PCQueue();
+
+        var work1 = new Work(new ConfigRecord(), new MockJourneyFileReader());
+        var work2 = new Work(new ConfigRecord(), new MockJourneyFileReader());
+
+        // Act
+        queue.enqueueItem(work1);
+        queue.enqueueItem(work2);
+
+        var dequeuedWork1 = queue.dequeueItem();
+        var dequeuedWork2 = queue.dequeueItem();
+
+        // Assert
+        Assert.AreEqual(work1, dequeuedWork1);
+        Assert.AreEqual(work2, dequeuedWork2);
+    }
+
+    [TestMethod]
+    public void EnqueueItemAndDequeueItem_WithBoundedCapacity_ShouldSucceed()
+    {
+        // Arrange
+        var queue = new PCQueue(capacity: 1);
+
+        var work1 = new Work(new ConfigRecord(), new MockJourneyFileReader());
+        var work2 = new Work(new ConfigRecord(), new MockJourneyFileReader());
+
+        // Act
+        queue.enqueueItem(work1);
+
+        // Enqueuing the second item should block until the first item is dequeued
+        Task.Factory.StartNew(() =>
+        {
+            Task.Delay(500).Wait();
+            queue.enqueueItem(work2);
+        });
+
+        var dequeuedWork1 = queue.dequeueItem();
+        var dequeuedWork2 = queue.dequeueItem();
+
+        // Assert
+        Assert.AreEqual(work1, dequeuedWork1);
+        Assert.AreEqual(work2, dequeuedWork2);
+    }
+
+    [TestMethod]
+    public void DequeueItem_WhenQueueIsEmpty_ShouldBlock()
+    {
+        // Arrange
+        var queue = new PCQueue();
+
+        var work = new Work(new ConfigRecord(), new MockJourneyFileReader());
+
+        // Act
+        Task.Factory.StartNew(() =>
+        {
+            Task.Delay(500).Wait();
+            queue.enqueueItem(work);
+        });
+
+        // The dequeue should block until an item is enqueued
+        var dequeuedWork = queue.dequeueItem();
+
+        // Assert
+        Assert.AreEqual(work, dequeuedWork);
+    }
+
+
+    //Mock class for IJourney
+    private class MockJourneyFileReader : IJourneyFileReader
+    {
+        public Journey ReadJourneyDataFromFile(ConfigRecord configRecord)
+        {
+            return new Journey("Test Journey");
+        }
+    }
+}
